83aa15ecc588110902258c7c87d30ebc52819ed0
Saqib Hasib <saqibhasib@gmail.com>
2021-07-23 00:25:51 -0700

    setup look of search bar

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { NavBar } from "./NavBar/NavBar";
+import { SearchBar } from "../SearchBar/SearchBar";
+
+import logo from "../assets/logo.png";
+import styles from "./HomePage.module.css";
 
 export function HomePage() {
     return (
         <div>
             <NavBar/>
+            <img src={logo} className={styles.logo} alt="logo"/>
+            <SearchBar/>
         </div>
     );
 };
---
+++ src/SearchBar/SearchBar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styles from './SearchBar.module.css';
+
+
+export function SearchBar() {
+    return(
+        <div>
+            <div className="field has-addons">
+                <p className="control">
+                    <button className="button is-static">
+                        Search
+                    </button>
+                </p>
+                <p className="control">
+                    <input className={`input ${styles['input-control']}`} type="text" placeholder="restaurants, movers ..."/>
+                </p>
+                <p className="control">
+                    <button className="button is-static">
+                        Near
+                    </button>
+                </p>
+                <p className="control">
+                    <input className={`input ${styles['input-control']}`} type="text" placeholder="Where"/>
+                </p>
+                <button className={`button ${styles["search-button"]}`}>
+                    <span className="icon is-small">
+                    <i className="fas fa-search"></i>
+                    </span>
+                </button>
+            </div>
+        </div>
+    );
+}
