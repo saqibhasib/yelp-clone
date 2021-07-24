f27726e7242f64fc7b2c07a57ae5e7f70e157fbd
Saqib Hasib <saqibhasib@gmail.com>
2021-07-23 19:38:09 -0700

    add routing to home page's search bar and logos

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -5,14 +5,27 @@ import { SearchSuggestion } from "./SearchSuggestion/SearchSuggestion";
 
 import logo from "../assets/logo.png";
 import styles from "./HomePage.module.css";
+import { Link, useHistory } from "react-router-dom";
 
 export function HomePage() {
+    
+    const history = useHistory();
+
+    function search(term, location){
+        const urlEncodedTerm = encodeURI(term);
+        const urlEncodedLocation = encodeURI(location);
+
+        history.push(`/search?find_desc=${urlEncodedTerm}&find_loc=${urlEncodedLocation}`);
+    }
+
     return (
         <div className={styles["home"]}>
             <div className={styles["search-area"]}>
                 <NavBar/>
-                <img src={logo} className={styles.logo} alt="logo"/>
-                <SearchBar/>
+                <Link to='/'>
+                    <img src={logo} className={styles.logo} alt="logo"/>
+                </Link>
+                <SearchBar search={search}/>
                 <SearchSuggestion/>
             </div>
         </div>
--- src/NavBarMain/NavBarMain.js
+++ src/NavBarMain/NavBarMain.js
@@ -2,11 +2,14 @@ import React from 'react';
 import logo from '../assets/logo.png';
 import { SearchBar } from '../SearchBar/SearchBar';
 import styles from './NavBarMain.module.css';
+import { Link } from 'react-router-dom';
 
 export function NavBarMain(){
     return(
         <div className={styles['nav-bar']}>
-            <img src={logo} className={styles.logo} alt="logo"/>
+            <Link to='/'>
+                <img src={logo} className={styles.logo} alt="logo"/>
+            </Link>
             <SearchBar small/>
             
             <button className={`button ${styles['nav-button']}`}>Sign In</button>
